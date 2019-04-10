f2869e16e57a413b4679b3c65127483e804caf49
Maxime Malbrancq <maximemalbrancq@Mac-Pro-de-Maxime-Malbrancq.local>
2019-04-11 00:42:25 +0200

    css et caroussel mais ne fonctionne toujours pas

--- view/backend/menu.php
+++ view/backend/menu.php
@@ -7,52 +7,53 @@ $title = 'Menu Naturopathe';
 ?>
 
 <div id="breadcrumb-container" class="container">
-		<div class="row">
-			<div class="col">
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="#">Accueil</a></li>
-						<!-- <li class="breadcrumb-item"><a href="#"></a></li> -->
-						<li class="breadcrumb-item active" aria-current="page">Plus jamais fatigué et détox des métaux lourds</li>
+    <div class="row">
+        <div class="col">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Accueil</a></li>
+                    <!-- <li class="breadcrumb-item"><a href="#"></a></li> -->
+                    <li class="breadcrumb-item active" aria-current="page">Plus jamais fatigué et détox des métaux lourds</li>
 
-					</ol>
-				</nav>
-			</div>
-		</div>
-	</div>
-    <!--------------------------------------------- PARTIE HAUTE ------------------------------------------------>
-    <div id="partie haute" class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="card" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title">Êtes-vous intoxiqué aux métaux lourds ?</h5>
-                        <p class="card-text">Comment savoir si vous êtes intoxiqués par les métaux lourds sans faire de test coûteux ou dangereux ? 
-                        Je vous présente un test gratuit qui permet de vous faire une très bonne idée de votre niveau d’intoxication.</p>
-                        <a href="#" class="btn btn-primary d-block">ACCÉDER AU TEST</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 "><iframe width="100%" height="100%" src="https://www.youtube.com/embed/kQGvyL56R6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>             
-            </div>
+                </ol>
+            </nav>
         </div>
     </div>
-    <!--------------------------------------------- Séparation HR ------------------------------------------------>
-    <hr id="hr1">
+</div>
+<!--------------------------------------------- PARTIE HAUTE ------------------------------------------------>
+<div id="partie haute" class="container">
+    <div class="row">
+        <div class="col-12 col-md-6 d-flex justify-content-center">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">Êtes-vous intoxiqué aux métaux lourds ?</h5>
+                    <p class="card-text">Comment savoir si vous êtes intoxiqués par les métaux lourds sans faire de test coûteux ou dangereux ?
+                        Je vous présente un test gratuit qui permet de vous faire une très bonne idée de votre niveau d’intoxication.</p>
+                    <a href="#" class="btn btn-primary d-block">ACCÉDER AU TEST</a>
+                </div>
+            </div>
+        </div>
+        <div id="videoMenu" class="col-12 col-md-6 d-flex justify-content-center"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/kQGvyL56R6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+<!--------------------------------------------- Séparation HR ------------------------------------------------>
+<hr id="hr1">
 
-    <!--------------------------------------------- PARTIE BASSE ------------------------------------------------>
+<!--------------------------------------------- PARTIE BASSE ------------------------------------------------>
+<!--
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+    //------ Indicators ------
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
-
-    <!-- Wrapper for slides -->
+    
+    //------ Wrapper for slides ------
     <div class="carousel-inner">
         <div class="item active">
-        <img src="images/Detox-Diets-For-Weight-Loss.-500x500" alt="Des jus de légumes pour plus de jus">
+        <img src="images/Detox-Diets-For-Weight-Loss.-500x500.jpg" alt="Des jus de légumes pour plus de jus">
         </div>
 
         <div class="item">
@@ -60,10 +61,10 @@ $title = 'Menu Naturopathe';
         </div>
 
         <div class="item">
-        <img src="Smoothie-aux-fruits-500x346.jpg" alt="Mon Smoothie ô fruits">
+        <img src="images/Smoothie-aux-fruits-500x346.jpg" alt="Mon Smoothie ô fruits">
         </div>
     </div>
-    <!-- Left and right controls -->
+    //------ Left and right controls ------
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
         <span class="sr-only">Previous</span>
@@ -72,8 +73,45 @@ $title = 'Menu Naturopathe';
         <span class="glyphicon glyphicon-chevron-right"></span>
         <span class="sr-only">Next</span>
     </a>
+    </div> 
+-->
+    <div id="my_carousel" class="carousel slide container" data-ride="carousel">
+        <!-- Bulles -->
+        <ol class="carousel-indicators">
+            <li data-target="#my_carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#my_carousel" data-slide-to="1"></li>
+            <li data-target="#my_carousel" data-slide-to="2"></li>
+        </ol>
+        <!-- Slides -->
+        <div class="carousel-inner">
+            <!-- Page 1 -->
+            <div class="item active">
+                <div class="carousel-page">
+                    <img src="images/Detox-Diets-For-Weight-Loss.-500x500.jpg" class="img-responsive" style="margin:0px auto;" />
+                </div>
+                <div class="carousel-caption">Page 1 de présentation</div>
+            </div>
+            <!-- Page 2 -->
+            <div class="item">
+                <div class="carousel-page"><img src="images/Stop-a-la-fatigue-couv-500x500.png" class="img-responsive img-rounded" style="margin:0px auto;" /></div>
+                <div class="carousel-caption">Page 2 de présentation</div>
+            </div>
+            <!-- Page 3 -->
+            <div class="item">
+                <div class="carousel-page">
+                    <img src="images/Smoothie-aux-fruits-500x346.jpg" class="img-responsive img-rounded" style="margin:0px auto;max-height:100%;" />
+                </div>
+                <div class="carousel-caption">Page 2 de présentation</div>
+            </div>
+        </div>
+        <!-- Contrôles -->
+        <a class="left carousel-control" href="#my_carousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#my_carousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
     </div>
-
     <?php
 
     $content = ob_get_clean();
--- view/template.php
+++ view/template.php
@@ -6,79 +6,96 @@
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-	 crossorigin="anonymous" />
-	 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700" rel="stylesheet"> 	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700" rel="stylesheet">
 	<link rel="stylesheet" href="index.css">
+	<style>
+		@media screen and (max-width : 767px) {
+			#videoMenu {
+				height: 280px;
+				margin-top: 15px;
+			}
+		}
+
+		.deconnexion {
+			color: rgba(0, 0, 0, .5);
+		}
+
+		.carousel-page {
+			width: 100%;
+			height: 346px;
+			background-color: #5f666d;
+			color: white;
+		}
+	</style>
 </head>
 
-	<header id="site-header" class="mb-3">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
-						<a class="navbar-brand" href="#">Le NATUROPATHE</a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-						 aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-
-						<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-							<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-								<li class="nav-item active d-flex align-items-center">
-									<a class="nav-link" href="#">FATIGUE<span class="sr-only">(current)</span></a>
-								</li>
-								<li class="nav-item d-flex align-items-center">
-									<a class="nav-link" href="#">METAUX-LOURDS</a>
-								</li>
-								<li class="nav-item d-flex align-items-center">
-									<a class="nav-link" href="#">PRENDRE RENDEZ-VOUS</a>
-								</li>
-								<li>
-                                	<form method="post">
-                                		<button class="btn btn-default" type="submit" name="logout" value="1">Déconnexion</button>
-                                	</form>
-                                </li>
-								<li class="nav-item d-flex align-items-center">
-                                    <a class="nav-link" href="#"><img src="images/icon1.png"></a>
-                                </li>
-                                
-							</ul>
-
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</header>
-    <body>
-	<?= $content ?>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    </body>
-	<footer class="container mb-3">
+<header id="site-header" class="mb-3">
+	<div class="container">
 		<div class="row">
 			<div class="col">
-				<div class="card">
-					<div class="card-body row">
-						<div class="col-md-9">
-							<div id="logo" class="d-none d-md-block"><a href="index.html">
-									<h4 class="align-items-center mb-0">Le NATUROPATHE</h4>
-								</a>
-							</div>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">Le NATUROPATHE</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+						<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+							<li class="nav-item active d-flex align-items-center">
+								<a class="nav-link" href="#">FATIGUE<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item d-flex align-items-center">
+								<a class="nav-link" href="#">METAUX-LOURDS</a>
+							</li>
+							<li class="nav-item d-flex align-items-center">
+								<a class="nav-link" href="#">PRENDRE RENDEZ-VOUS</a>
+							</li>
+							<li class="d-flex align-items-center">
+								<form method="post">
+									<button class="btn btn-default deconnexion" type="submit" name="logout" value="1">DECONNEXION</button>
+								</form>
+							</li>
+							<!--<li class="nav-item d-flex align-items-center">
+                                    <a class="nav-link" href="#"><img src="images/icon1.png"></a>
+                                </li>-->
+
+						</ul>
+
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+
+<body>
+	<?= $content ?>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+<footer class="container mb-3">
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-body row">
+					<div class="col-md-9">
+						<div id="logo" class="d-none d-md-block"><a href="index.html">
+								<h4 class="align-items-center mb-0">Le NATUROPATHE</h4>
+							</a>
 						</div>
-						<div class="col-md-3  d-flex justify-content-center justify-content-md-end">
-							<ul class="socials d-flex align-items-center">
-								<li class=""><a href="#"><img src="images/icon-facebook.svg" alt=""></a></li>
-								<li class=""><a href="#"><img src="images/icon-twitter.svg" alt=""></a></li>
-                                <li class=""><a href="#"><img src="images/icon-youtube.svg" alt=""></a></li>
-							</ul>
-						</div>
+					</div>
+					<div class="col-md-3  d-flex justify-content-center justify-content-md-end">
+						<ul class="socials d-flex align-items-center">
+							<li class=""><a href="#"><img src="images/icon-facebook.svg" alt=""></a></li>
+							<li class=""><a href="#"><img src="images/icon-twitter.svg" alt=""></a></li>
+							<li class=""><a href="#"><img src="images/icon-youtube.svg" alt=""></a></li>
+						</ul>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 </footer>
-
